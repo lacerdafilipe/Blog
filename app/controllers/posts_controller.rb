a85844907df5e7982cd70750141434231bc1d45c
Filipe Lacerda <filipelacerda@Filipes-MacBook-Pro.local>
2017-09-27 22:16:21 +0100

    Add a partial form and edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,20 @@ class PostsController < ApplicationController
   	
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+      if @post.update(params[:post].permit(:title, :body))
+          redirect_to @post
+      else
+        render 'edit'
+      end 
+  end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
