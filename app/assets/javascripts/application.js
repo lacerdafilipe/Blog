3daa9dc08ce2ed3e180e71782b6e27f539f0c7da
Filipe Lacerda <filipelacerda@Filipes-MacBook-Pro.local>
2017-09-28 00:56:39 +0100

    add comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require materialize-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
